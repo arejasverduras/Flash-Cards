5fa214d2296eef501724f38fb1116a1da7919b1e
Arejas <62893479+arejasverduras@users.noreply.github.com>
2022-05-31 12:57:33 +0200

    add topics key to initial selector. works by selecting the .topics key in topics.js

--- src/features/topics/topicsSlice.js
+++ src/features/topics/topicsSlice.js
@@ -3,12 +3,12 @@ import { createSlice } from '@reduxjs/toolkit';
 const topicsSlice = createSlice({
     name: 'topics',
     initialState: {
-        
+        topics: {}
     },
     reducers: {
         addTopic: (state, action) => 
         {
-            state[action.payload.id] = {
+            state.topics[action.payload.id] = {
                 id: action.payload.id,
                 name: action.payload.name,
                 icon: action.payload.icon,
